c0f5889c1c4d786ebe793cc44273f287cb9d64cf
komkid <komkid@gmail.com>
2016-11-26 16:43:08 +0700

    Create BasicOTA.ino

---
+++ BasicOTA.ino
@@ -0,0 +1,122 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "...";
+const char* password = "...";
+
+#include <time.h>
+int timezone = 7 * 3600;
+int dst = 0;
+struct tm* p_tm;
+unsigned int h;
+unsigned int m;
+unsigned int s;
+
+void printTimeNow(){
+  Serial.println();
+  Serial.print(h); // print the hour (86400 equals secs per day)
+  Serial.print(':');
+  if ( m < 10 ) {
+    // In the first 10 minutes of each hour, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print(m); // print the minute (3600 equals secs per minute)
+  Serial.print(':');
+  if ( s < 10 ) {
+    // In the first 10 seconds of each minute, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print(s); // print the second
+  Serial.print(" >> "); // print the second
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+   ArduinoOTA.setPassword("...");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  configTime(timezone, dst, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println();
+  time_t now = time(nullptr);
+  p_tm = localtime(&now);
+  h = p_tm->tm_hour;
+  m = p_tm->tm_min;
+  s = p_tm->tm_sec;
+
+  printTimeNow();
+  Serial.println("Now");
+}
+
+void loop() {
+  ArduinoOTA.handle();
+
+  s = s + 1;
+  if((s / 60) > 0) {
+     s = s % 60;
+     m++;
+     if(m == 60){
+       m = 0;
+       h++;
+       if(h == 24) h = 0;
+     }
+  }
+  printTimeNow();
+  delay(1000);
+}
